5a5253c6152ba028a55f2215b9a6408277f0c011
JotaCe7 <jcisneros@meicaperu.com>
2022-11-10 12:17:38 -0500

    implement detectron2 model predictions

--- utils/detection.py
+++ utils/detection.py
@@ -3,10 +3,9 @@ from detectron2.config import get_cfg
 from detectron2.engine.defaults import DefaultPredictor
 
 # The chosen detector model is "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
-# because this particular model has a good balance between accuracy and speed.
-# You can check the following Colab notebook with examples on how to run
-# Detectron2 models
-# https://colab.research.google.com/drive/16jcaJoc6bCFAQ96jDe2HwtXj7BMD_-m5.
+# because it has a good balance between accuracy and speed.
+# User guide: https://colab.research.google.com/drive/16jcaJoc6bCFAQ96jDe2HwtXj7BMD_-m5.
+
 # Assign the loaded detection model to global variable DET_MODEL
 cfg = get_cfg()
 cfg.merge_from_file(
@@ -35,18 +34,6 @@ def get_vehicle_coordinates(img):
     variable) over the the image, get the vehicle position in the picture
     and return it.
 
-    Many things should be taken into account to make it work:
-        1. Current model being used can detect up to 80 different objects,
-           we're only looking for 'cars' or 'trucks', so you should ignore
-           other detected objects.
-        2. The object detector may find more than one vehicle in the picture,
-           you must then, choose the one with the largest area in the image.
-        3. The model can also fail and detect zero objects in the picture,
-           in that case, you should return coordinates that cover the full
-           image, i.e. [0, 0, width, height].
-        4. Coordinates values must be integers, we're making reference to
-           a position in a numpy.array, we can't use float values.
-
     Parameters
     ----------
     img : numpy.ndarray
@@ -58,7 +45,23 @@ def get_vehicle_coordinates(img):
         List having bounding box coordinates as [left, top, right, bottom].
         Also known as [x1, y1, x2, y2].
     """
-    # TODO
-    box_coordinates = None
+      
+    # COC Classnames https://gist.github.com/AruniRC/7b3dadd004da04c80198557db5da4bda
+
+    # Set default corrdinates to cover the full image
+    h, w = img.shape[:2]
+    box_coordinates =  [0, 0, w, h]
+
+    # Get instances detected on img using our model
+    outputs = DET_MODEL(img)
+    instances = outputs["instances"]
+    
+    # Filter instances corresponding to cars and truckss
+    vehicle_instances = instances[(instances.pred_classes == 2) | (instances.pred_classes == 7)]
+    if len(vehicle_instances):
+      # If there is at least one car or trcuk, get the coordinates for the biggest Box
+      biggest_idx_box = vehicle_instances.pred_boxes.area().argmax().item()
+      box_coordinates = vehicle_instances.pred_boxes[biggest_idx_box].tensor[0].numpy()   
 
     return box_coordinates
+
